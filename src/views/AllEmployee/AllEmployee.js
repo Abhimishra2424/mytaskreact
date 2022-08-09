8a8bc24b51989d7217fe0f423e6f94b9a5370a24
Abhishek Mishra <abmishra700@gmail.com>
2022-08-09 11:50:43 +0530

    added Typo

--- src/views/AllEmployee/AllEmployee.js
+++ src/views/AllEmployee/AllEmployee.js
@@ -11,8 +11,17 @@ import TableRow from "@material-ui/core/TableRow";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useAppContext } from "../../context/appContext";
-import { Button } from "@material-ui/core";
-import EditIcon from '@material-ui/icons/Edit';
+import { Button, TextField } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 const useStyles = makeStyles({
     table: {
         minWidth: 450,
@@ -23,71 +32,141 @@ const AllEmployee = () => {
     const { company } = useAppContext();
     const classes = useStyles();
 
-    const [data, setdata] = useState([])
+    const [data, setdata] = useState([]);
     const [page, setpage] = useState(0);
     const [rowsPerPage, setrowsPerPage] = useState(5);
+
+    const [open, setOpen] = React.useState(false);
+
+const [selectedData, setSelectedData] = useState({})
+
 
     useEffect(() => {
         const getAllemployee = async () => {
             let payload = {
-                company_id: company.company_id
-            }
-            const { data } = await axios.post("http://localhost:5000/api/company/getAllEmployeeByCompanyId", payload);
-            console.log(data)
+                company_id: company.company_id,
+            };
+            const { data } = await axios.post(
+                "http://localhost:5000/api/company/getAllEmployeeByCompanyId",
+                payload
+            );
+            console.log(data);
             setdata(data.employees);
-        }
+        };
         getAllemployee();
-    }, [company.company_id])
+    }, [company.company_id]);
 
     const handleChangePage = (event, newPage) => {
         setpage(newPage);
-    }
+    };
     const handleChangeRowsPerPage = (event) => {
         setrowsPerPage(+event.target.value);
         setpage(0);
+    };
+
+
+    const handleClickOpen = () => {
+        setOpen(true);
+
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleEdit = (e , data) =>{
+        console.log(data)
+        setOpen(true);
+        setSelectedData(data)
     }
+
+
 
     return (
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
-                    <TableRow 
+                    <TableRow
                         style={{
-                            backgroundColor: "#f5f5f5",
+                            backgroundColor: "#3D6889",
                             fontWeight: "bold",
                             fontSize: "1.2rem",
-                            color: "#000"
-
                         }}
-
                     >
-                        <TableCell align="right">Employee Id</TableCell>
-                        <TableCell align="right">Employee Name</TableCell>
-                        <TableCell align="right">Employee Email</TableCell>
-                        <TableCell align="right">Employee Role</TableCell>
-                        <TableCell align="right">Action</TableCell>
+                        <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Employee Code
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Employee Name
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Employee Email
+                         </TableCell>
+                         <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Employee Password
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Employee Role
+                        </TableCell>
+                        <TableCell
+                            style={{
+                                color: "white",
+                            }}
+                            align="right"
+                        >
+                            Action
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {data?.length
-                        ? data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        .map((row) => (
-                            <TableRow key={row.employee_id}>
-                                <TableCell align="right">{row.employee_id}</TableCell>
-                                <TableCell align="right">{row.employeeName}</TableCell>
-                                <TableCell align="right">{row.employeeEmail}</TableCell>
-                                <TableCell align="right">{row.employeeRole}</TableCell>
-                                <TableCell align="right">
-                                    <Link to={`/employee/${row.employee_id}`}>
-                                        <Button variant="outlined" color="primary" startIcon={<EditIcon />}>
+                        ? data
+                            ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => (
+                                <TableRow key={row.employee_id}>
+                                    <TableCell align="right">{row.employeeCode}</TableCell>
+                                    <TableCell align="right">{row.employeeName}</TableCell>
+                                    <TableCell align="right">{row.employeeEmail}</TableCell>
+                                    <TableCell align="right">{row.employeePassword}</TableCell>
+                                    <TableCell align="right">{row.employeeRole}</TableCell>
+                                    <TableCell align="right">
+                                        <Button
+                                            variant="outlined"
+                                            color="primary"
+                                            startIcon={<EditIcon />}
+                                            onClick={(e)=>handleEdit(e, row)}
+                                        >
                                             Edit
                                         </Button>
-                                    </Link>
-                                </TableCell>
-                            </TableRow>
-                        ))
+                                    </TableCell>
+                                </TableRow>
+                            ))
                         : "no data"}
-
                 </TableBody>
             </Table>
             <TablePagination
@@ -99,6 +178,82 @@ const AllEmployee = () => {
                 onChangePage={handleChangePage}
                 onChangeRowsPerPage={handleChangeRowsPerPage}
             />
+
+            {/* Edit Modal */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Edit Employee"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <form >
+                            <TextField
+                                id="outlined-basic"
+                                label="Employee Code"
+                                variant="outlined"
+                                fullWidth
+                                value={selectedData.employeeCode}
+                                onChange={(e)=>setSelectedData({...selectedData, employeeCode: e.target.value})}
+                                disabled
+
+                                margin="normal"
+                            />
+
+                            <TextField
+                                id="outlined-basic"
+                                label="Employee Name"
+                                variant="outlined"
+                                fullWidth
+                                value={selectedData.employeeName}
+                                onChange={(e)=>setSelectedData({...selectedData, employeeName: e.target.value})}
+                                margin="normal"
+                            />
+                            <TextField
+                                id="outlined-basic"
+                                label="Employee Email"
+
+                                variant="outlined"
+                                fullWidth
+                                value={selectedData.employeeEmail}
+                                onChange={(e)=>setSelectedData({...selectedData, employeeEmail: e.target.value})}
+                                margin="normal"
+
+                            />
+                            <TextField
+                                id="outlined-basic"
+                                label="Employee Password"
+                                variant="outlined"
+                                fullWidth
+                                value={selectedData.employeePassword}
+                                onChange={(e)=>setSelectedData({...selectedData, employeePassword: e.target.value})}
+                                margin="normal"
+
+
+                            />
+                           
+                            <Button
+                                variant="contained"
+                                fullWidth
+                                type="submit"
+                                color='primary'
+                            >
+                                Update
+                            </Button>
+                        </form>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </TableContainer>
     );
 };
