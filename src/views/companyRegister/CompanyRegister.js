48d3d78b419de8e7ae55a8b15bbbe8327ff6cab1
Abhishek Mishra <abmishra700@gmail.com>
2022-09-18 21:28:06 +0530

    logo change

--- src/views/companyRegister/CompanyRegister.js
+++ src/views/companyRegister/CompanyRegister.js
@@ -1,14 +1,15 @@
 import { Button, makeStyles, TextField } from "@material-ui/core";
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom"
 import { Link } from "react-router-dom";
-import LockOpenIcon from '@material-ui/icons/LockOpen';
-import RealMeLogo from '../../../src/realbooks.png'
 
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { useAppContext } from "../../context/appContext";
 
-import { useHistory } from "react-router-dom"
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const useStyles = makeStyles({
   button: {
@@ -53,26 +54,11 @@ const CompanyRegister = () => {
           <div className="px-6 w-full flex flex-wrap items-center justify-between">
             <div className="flex items-center">
               <Link className="navbar-brand text-blue-600" to="/">
-                <img src={RealMeLogo} alt="RealMe" style={{
-                  width: '40px',
-                  height: '40px',
-                }}
-                />
+                     <AssignmentIcon  fontSize='large'  />
               </Link>
-            </div>
-            <div className="flex items-center items-center lg:ml-auto">
-              <Button
-                type="button"
-                className={classes.button}
-                variant="contained"
-                color="primary"
-                size="large"
-                startIcon={<LockOpenIcon />}
-              ><Link to="/login">Login</Link></Button>
             </div>
           </div>
         </nav>
-
         <div className="px-6 py-12 md:px-12 bg-gray-100 text-gray-800 text-center lg:text-left">
           <div className="container mx-auto xl:px-32">
             <div className="grid lg:grid-cols-2 gap-12 flex items-center">
